86e7b451720d66d23fa3fc433269281b34a80c2e
John Richardson <jrichardson@ncsasports.org>
2012-11-10 22:25:28 -0600

    removes autotest dependency since I couldn't get it to work anyway

--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject bowling-kata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Uncle Bob's Bowling Kata (in Clojure)"
+  :url "https://github.com/barooo/bowling-kata-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]  
-		 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]
-		     [lein-autotest "1.1.0"]]
-  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
+  :dependencies [[org.clojure/clojure "1.4.0"]])
